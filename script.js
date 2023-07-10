5c706cf1733f2dd2c93faea8525d0f9eaacb141f
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-07-10 18:54:04 +0300

    6

--- script.js
+++ script.js
@@ -30,3 +30,48 @@ function century(year) {
 }
 
 console.log(century(243));
+
+// 4. Напишите функцию для преобразования имени в инициалы. Это ката строго принимает два слова с одним пробелом между ними.
+
+// Выходные данные должны состоять из двух заглавных букв с разделяющей их точкой.
+
+// Это должно выглядеть примерно так:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function some (name) {
+    const [first, last] = name.split(' ');
+    return `${first[0].toUpperCase()}.${last[0].toUpperCase()}`;
+
+    // v2
+    // const nameArray = name.split(" ");
+    // return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+console.log(some('Sam Harris'));
+
+// 5. Завершите решение так, чтобы оно перевернуло переданную в него строку.
+
+function solution(str){
+    return str.split('').reverse().join('');
+}
+
+console.log(solution('maxim'));
+
+// 6 .Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+function maskify(str) {
+    if (str.length <= 4) {
+        return str;
+      }
+    
+      const masked = "#".repeat(str.length - 4) + str.slice(-4);
+      return masked;
+}
+
+console.log(maskify('4556364607935616'));
+
